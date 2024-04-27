b621695d508adc957b1a184a4d94922bdec18266
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-04-27 23:06:50 +0200

    clean up of dex page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,17 +18,6 @@ const monserrat = Montserrat({
 })
 
 export default function Home() {
-    const [connected, setConnected] = useState(false)
-
-    // Mainnet
-    const [chosenTokenInput, chooseTokenInput] = useState("Symbol")
-    const [chosenTokenOutput, chooseTokenOutput] = useState("Symbol")
-    const [chosenTokenAddressInput, chooseTokenAddressInput] = useState(0)
-    const [chosenTokenAddressOutput, chooseTokenAddressOutput] = useState(0)
-    const [chosenTokenDecimalsInput, chooseTokenDecimalsInput] = useState(0)
-    const [chosenTokenDecimalsOutput, chooseTokenDecimalsOutput] = useState(0)
-    const [amountOutput, changeAmountOutput] = useState(0)
-    const [inputAmount, setInputAmount] = useState(0)
     const [isMainnet, setIsMainnet] = useState(false) // to switch between mainnet and testnet tokens
 
     // Testnet tokens(liquidityPool)
@@ -44,36 +33,22 @@ export default function Home() {
     return (
         <div className={monserrat.className}>
             <main className="flex justify-center">
-                <div
-                    className={`${
-                        isMainnet ? "shadow-cyan-500" : "shadow-yellow-500"
-                    } w-[450px] space-y-1 mt-16 py-10 text-gray-400 p-10 text-center flex flex-col rounded-xl shadow-2xl transition duration-200`}
-                >
+                <div className="shadow-cyan-500 w-[450px] space-y-1 mt-16 py-10 text-gray-400 p-10 text-center flex flex-col rounded-xl shadow-2xl transition duration-200">
                     <p className="text-2xl mb-4">Exchange Tokens</p>
                     <div className="flex flex-col">
                         <div>
                             <p className="ml-2 text-left">Input</p>
                         </div>
-                        {isMainnet ? (
-                            <Input
-                                chooseTokenInput={chooseTokenInput}
-                                chosenTokenInput={chosenTokenInput}
-                                chooseTokenAddressInput={chooseTokenAddressInput}
-                                chooseTokenDecimalsInput={chooseTokenDecimalsInput}
-                                setInputAmount={setInputAmount}
-                            ></Input>
-                        ) : (
-                            <InputTestnet
-                                chosenTokenInputTest={chosenTokenInputTest}
-                                chooseTokenInputTest={chooseTokenInputTest}
-                                chosenTokenAddressInputTest={chosenTokenAddressInputTest}
-                                chooseTokenAddressInputTest={chooseTokenAddressInputTest}
-                                chooseTokenDecimalsInputTest={chooseTokenDecimalsInputTest}
-                                chosenTokenDecimalsInputTest={chosenTokenDecimalsInputTest}
-                                setInputAmountTest={setInputAmountTest}
-                                isMainnet={isMainnet}
-                            />
-                        )}
+                        <InputTestnet
+                            chosenTokenInputTest={chosenTokenInputTest}
+                            chooseTokenInputTest={chooseTokenInputTest}
+                            chosenTokenAddressInputTest={chosenTokenAddressInputTest}
+                            chooseTokenAddressInputTest={chooseTokenAddressInputTest}
+                            chooseTokenDecimalsInputTest={chooseTokenDecimalsInputTest}
+                            chosenTokenDecimalsInputTest={chosenTokenDecimalsInputTest}
+                            setInputAmountTest={setInputAmountTest}
+                            isMainnet={isMainnet}
+                        />
                     </div>
                     <div className="flex justify-center">
                         <Image
@@ -88,54 +63,27 @@ export default function Home() {
                         <div>
                             <p className="ml-2 text-left">Output</p>
                         </div>
-                        {isMainnet ? (
-                            <Output
-                                chooseTokenOutput={chooseTokenOutput}
-                                chosenTokenOutput={chosenTokenOutput}
-                                chosenTokenAddressInput={chosenTokenAddressInput}
-                                chooseTokenAddressOutput={chooseTokenAddressOutput}
-                                chosenTokenAddressOutput={chosenTokenAddressOutput}
-                                chosenTokenDecimalsInput={chosenTokenDecimalsInput}
-                                chooseTokenDecimalsOutput={chooseTokenDecimalsOutput}
-                                chosenTokenDecimalsOutput={chosenTokenDecimalsOutput}
-                                changeAmountOutput={changeAmountOutput}
-                                inputAmount={inputAmount}
-                                amountOutput={amountOutput}
-                            ></Output>
-                        ) : (
-                            <OutputTestnet
-                                chosenTokenOutputTest={chosenTokenOutputTest}
-                                chooseTokenOutputTest={chooseTokenOutputTest}
-                                chosenTokenAddressOutputTest={chosenTokenAddressOutputTest}
-                                chooseTokenAddressOutputTest={chooseTokenAddressOutputTest}
-                                chooseTokenDecimalsOutputTest={chooseTokenDecimalsOutputTest}
-                                chosenTokenDecimalsOutputTest={chosenTokenDecimalsOutputTest}
-                                inputAmountTest={inputAmountTest}
-                                setOutputAmountTest={setOutputAmountTest}
-                                chosenTokenAddressInputTest={chosenTokenAddressInputTest}
-                                outputAmountTest={outputAmountTest}
-                            />
-                        )}
-                    </div>
-                    {isMainnet ? (
-                        <Swap
-                            chosenTokenAddressInput={chosenTokenAddressInput}
-                            chosenTokenAddressOutput={chosenTokenAddressOutput}
-                            chosenTokenDecimalsInput={chosenTokenDecimalsInput}
-                            chosenTokenDecimalsOutput={chosenTokenDecimalsOutput}
-                            inputAmount={inputAmount}
-                            connected={connected}
-                        ></Swap>
-                    ) : (
-                        <SwapTest
-                            chosenTokenAddressInputTest={chosenTokenAddressInputTest}
+                        <OutputTestnet
+                            chosenTokenOutputTest={chosenTokenOutputTest}
+                            chooseTokenOutputTest={chooseTokenOutputTest}
                             chosenTokenAddressOutputTest={chosenTokenAddressOutputTest}
+                            chooseTokenAddressOutputTest={chooseTokenAddressOutputTest}
+                            chooseTokenDecimalsOutputTest={chooseTokenDecimalsOutputTest}
                             chosenTokenDecimalsOutputTest={chosenTokenDecimalsOutputTest}
-                            chosenTokenDecimalsInputTest={chosenTokenDecimalsInputTest}
                             inputAmountTest={inputAmountTest}
+                            setOutputAmountTest={setOutputAmountTest}
+                            chosenTokenAddressInputTest={chosenTokenAddressInputTest}
                             outputAmountTest={outputAmountTest}
-                        ></SwapTest>
-                    )}
+                        />
+                    </div>
+                    <SwapTest
+                        chosenTokenAddressInputTest={chosenTokenAddressInputTest}
+                        chosenTokenAddressOutputTest={chosenTokenAddressOutputTest}
+                        chosenTokenDecimalsOutputTest={chosenTokenDecimalsOutputTest}
+                        chosenTokenDecimalsInputTest={chosenTokenDecimalsInputTest}
+                        inputAmountTest={inputAmountTest}
+                        outputAmountTest={outputAmountTest}
+                    ></SwapTest>
                 </div>
             </main>
         </div>
