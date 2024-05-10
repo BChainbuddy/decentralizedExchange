7d9485ea6ba24e6b7c5831d003220ae1d76a69af
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-05-10 12:02:28 +0200

    added switch network option

--- src/components/Network.jsx
+++ src/components/Network.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
-import { useAccount } from "wagmi"
+import { useAccount, useSwitchChain } from "wagmi"
 
 export default function Network() {
-    const { chain } = useAccount()
+    const { chain, isConnected } = useAccount()
     const [clientChain, setClientChain] = useState(null)
 
     useEffect(() => {
@@ -11,22 +11,31 @@ export default function Network() {
         }
     }, [chain])
 
+    const { switchChain } = useSwitchChain()
+
+    const handleSwitchChain = () => {
+        if (isConnected && chain.name !== "Sepolia") {
+            switchChain({ chainId: 11155111 })
+        }
+    }
+
     return (
         <div
-            className={`transition duration-500 mr-2 border border-white rounded-md p-3 w-24 flex justify-center items-center overflow-hidden ${
+            className={`transition duration-500 mr-2 border border-white rounded-md w-24 flex justify-center items-center overflow-hidden text-center ${
                 !clientChain
-                    ? "bg-red-500 text-white"
+                    ? "bg-red-500 text-white cursor-pointer networkText"
                     : clientChain.name === "Sepolia"
-                    ? "bg-cyan-400 text-white"
-                    : "bg-red-500 text-white"
+                    ? "bg-cyan-400 text-white p-3"
+                    : "bg-red-500 text-white cursor-pointer networkText"
             }`}
+            onClick={handleSwitchChain}
         >
             <p>
                 {!clientChain
-                    ? "NETWORK"
-                    : clientChain.name === "Sepolia"
+                    ? "SWITCH NETWORK"
+                    : clientChain.name === "Sepolia" && isConnected
                     ? clientChain.name.toUpperCase()
-                    : "NETWORK"}
+                    : "SWITCH NETWORK"}
             </p>
         </div>
     )
